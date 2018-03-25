2641c5729206c57aa29b6d812e6a3c2ab7f4080a
Lucy Linder <lucy.derlin@gmail.com>
2018-03-25 16:14:23 +0200

    allow the reuse of an extractor object + fix getImages

--- src/boilerpipe/extract/__init__.py
+++ src/boilerpipe/extract/__init__.py
@@ -15,6 +15,23 @@ StringReader       = jpype.JClass('java.io.StringReader')
 HTMLHighlighter    = jpype.JClass('de.l3s.boilerpipe.sax.HTMLHighlighter')
 BoilerpipeSAXInput = jpype.JClass('de.l3s.boilerpipe.sax.BoilerpipeSAXInput')
 
+from functools import wraps
+
+def thread_safe(method):
+    @wraps(method)
+    def _impl(self, *args, **kwargs):
+        try:
+            # make it thread safe, see jpype documentation for more info
+            if threading.activeCount() > 1:
+                if jpype.isThreadAttachedToJVM() is False:
+                    jpype.attachThreadToJVM()
+            #lock.acquire()
+            return method(self, *args, **kwargs)
+        finally:
+            #lock.release()
+            pass
+    return _impl
+
 class Extractor(object):
     """
     Extract text. Constructor takes 'extractor' as a keyword argument,
@@ -33,65 +50,71 @@ class Extractor(object):
     data      = None
     headers   = {'User-Agent': 'Mozilla/5.0'}
     
+    @thread_safe
     def __init__(self, extractor='DefaultExtractor', **kwargs):
+        if extractor == "KeepEverythingWithMinKWordsExtractor":
+            kMin = kwargs.get("kMin", 1)  # set default to 1
+            self.extractor = jpype.JClass(
+                    "de.l3s.boilerpipe.extractors."+extractor)(kMin)
+        else:
+            self.extractor = jpype.JClass(
+                "de.l3s.boilerpipe.extractors."+extractor).INSTANCE
+
+    @thread_safe
+    def get(self, url=None, html=None):
+        return self._process(url=url, html=html)
+
+    @thread_safe
+    def getText(self, url=None, html=None):
+        print("getting text ", url)
+        source, data = self._process(html=html, url=url)
+        return source.getContent()
+
+    @thread_safe
+    def getHTML(self, url=None, html=None):
+        highlighter = HTMLHighlighter.newExtractingInstance()
+        source, data = self._process(self, url=url, html=html)
+        return highlighter.process(source, data)
+
+
+    def _process(self, **kwargs):
         if kwargs.get('url'):
             request     = urllib2.Request(kwargs['url'], headers=self.headers)
             connection  = urllib2.urlopen(request)
-            self.data   = connection.read()
+            data        = connection.read()
             encoding    = connection.headers['content-type'].lower().split('charset=')[-1]
             if encoding.lower() == 'text/html':
-                encoding = charade.detect(self.data)['encoding']
+                encoding = charade.detect(data)['encoding']
             if encoding is None:
                 encoding = 'utf-8'
-            self.data = str(self.data, encoding)
+            data = str(data, encoding)
         elif kwargs.get('html'):
-            self.data = kwargs['html']
-            if not isinstance(self.data, str):
-                self.data = str(self.data, charade.detect(self.data)['encoding'])
+            data = kwargs['html']
+            if not isinstance(data, str):
+                data = str(data, charade.detect(data)['encoding'])
         else:
             raise Exception('No text or url provided')
 
-        try:
-            # make it thread-safe
-            if threading.activeCount() > 1:
-                if jpype.isThreadAttachedToJVM() == False:
-                    jpype.attachThreadToJVM()
-            lock.acquire()
-            
-            if extractor == "KeepEverythingWithMinKWordsExtractor":
-                # handle argument
-                kMin = kwargs.get("kMin", 1)  # set default to 1
-                self.extractor = jpype.JClass(
-                        "de.l3s.boilerpipe.extractors."+extractor)(kMin)
-            else:
-                self.extractor = jpype.JClass(
-                    "de.l3s.boilerpipe.extractors."+extractor).INSTANCE
-        finally:
-            lock.release()
+        reader = StringReader(data)
+        source = BoilerpipeSAXInput(InputSource(reader)).getTextDocument()
+        self.extractor.process(source)
+        return source, data
     
-        reader = StringReader(self.data)
-        self.source = BoilerpipeSAXInput(InputSource(reader)).getTextDocument()
-        self.extractor.process(self.source)
-    
-    def getText(self):
-        return self.source.getContent()
-    
-    def getHTML(self):
-        highlighter = HTMLHighlighter.newExtractingInstance()
-        return highlighter.process(self.source, self.data)
-    
-    def getImages(self):
-        extractor = jpype.JClass(
-            "de.l3s.boilerpipe.sax.ImageExtractor").INSTANCE
-        images = extractor.process(self.source, self.data)
+    def getImages(self, source, data):
+        extractor = jpype.JClass("de.l3s.boilerpipe.sax.ImageExtractor").INSTANCE
+        images = extractor.process(source, data)
         jpype.java.util.Collections.sort(images)
-        images = [
-            {
-                'src'   : image.getSrc(),
-                'width' : image.getWidth(),
-                'height': image.getHeight(),
-                'alt'   : image.getAlt(),
-                'area'  : image.getArea()
-            } for image in images
-        ]
-        return images
+        # list comprehension returns 
+        #   TypeError: iter() returned non-iterator of type 'java.util.ArrayList$Itr'
+        # so do it the old way:
+        results = []
+        for i in range(images.size()):
+            img = images[i]
+            results.append({
+                'src'   : img.getSrc(),
+                'width' : img.getWidth(),
+                'height': img.getHeight(),
+                'alt'   : img.getAlt(),
+                'area'  : img.getArea()
+            })
+        return results
